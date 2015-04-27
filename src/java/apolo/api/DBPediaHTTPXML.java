f219eb04ba5ee9f4dae50d21bcb3db1f271ef56e
arcuellar88 <alejrodr@ulb.ac.be>
2015-04-27 17:47:51 +0200

    +Structure of the DBPedia Requests +IDBPedia method definition +gitIgnore
    #32110845846051

--- src/java/apolo/api/DBPediaHTTPXML.java
+++ src/java/apolo/api/DBPediaHTTPXML.java
@@ -16,18 +16,104 @@ import org.apache.http.impl.client.*;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-public class DBPediaHTTPXML {
+import apolo.entity.*;
+
+public class DBPediaHTTPXML implements IDBpedia {
 	
+	// --------------------------------------------------------
+	// Constants
+	// --------------------------------------------------------
+	private final static String HTTP_URL = "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryClass=QCLASS&QueryString=QUERYSTR";
+	private final static String QUERY_CLASS="QCLASS";
+	private final static String QUERY_STRING="QUERYSTR";
+	private final static String CLASS_ARTIST="Agent";
+
 	
-	
-	
-	public void testhttp()
+	// --------------------------------------------------------
+	// Attributes
+	// --------------------------------------------------------
+
+	// --------------------------------------------------------
+	// Constructor
+	// --------------------------------------------------------
+
+	public DBPediaHTTPXML()
 	{
+		
+	}
+	
+	// --------------------------------------------------------
+	// Methods
+	// --------------------------------------------------------
+
+	
+	@Override
+	public IArtist getAdditionalInformationArtist(IArtist a)
+	{
+		Element root=httpRequestDBPedia(CLASS_ARTIST, a.getName());
+		
+		if(root!=null)
+		{
+			// get all child nodes
+	        NodeList nodes = root.getChildNodes();
+
+	        /* print the text content of each child
+	        for (int i = 0; i < nodes.getLength(); i++) {
+	           System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).);
+	        }
+	        */
+			
+			Node nodo2=nodes.item(1); 
+			
+	        System.out.println(nodo2.getNodeName());
+
+	        
+	        nodes = nodo2.getChildNodes();
+	        
+	        for (int i = 0; i < nodes.getLength(); i++) {
+		            System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).getTextContent());
+		         }
+	        
+	        Node nodo3=nodes.item(6);
+	        System.out.println(nodo3.getNodeName());
+
+	        nodes = nodo3.getChildNodes();
+			      
+	        for (int i = 0; i < nodes.getLength(); i++) {
+		            System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).getTextContent());
+		         }
+		}        
+        
+		return a;
+	}
+	
+	public ISong getAdditionalInformationSong(ISong s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IRelease getAdditionalInformationRelease(IRelease r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	/**
+	 * Methods that transforms the HTTP GET request into a DOM element
+	 * @param qClass Type of the DBPedia Query: person, agent, release, etc. 
+	 * @param name Name of the entity: of the artist, song or release
+	 * @return The root element of the response from DBPedia
+	 */
+	public Element httpRequestDBPedia(String qClass,String name)
+	{
+		Element root=null;
 		try {
-			String url = "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryClass=Agent&QueryString=Shakira";
 			 
 			HttpClient client = HttpClientBuilder.create().build();
-			HttpGet request = new HttpGet(url);
+			String query=HTTP_URL.replaceAll(QUERY_CLASS, qClass);
+			
+			HttpGet request = new HttpGet(query.replaceAll(QUERY_STRING, name));
 		 
 			// add request header
 			//request.addHeader("User-Agent", USER_AGENT);
@@ -37,53 +123,12 @@ public class DBPediaHTTPXML {
 
 			response = client.execute(request);
 			
-			System.out.println("Response Code : " 
-		                + response.getStatusLine().getStatusCode());
-		 
 			
 			Document doc = builder.parse(response.getEntity().getContent());
 
 	         // get the first element
-	         Element element = doc.getDocumentElement();
+	         root = doc.getDocumentElement();
 
-	         // get all child nodes
-	         NodeList nodes = element.getChildNodes();
-
-	         /* print the text content of each child
-	         for (int i = 0; i < nodes.getLength(); i++) {
-	            System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).);
-	         }
-	         */
-			
-			Node nodo2=nodes.item(1); 
-			
-	         System.out.println(nodo2.getNodeName());
-
-	         
-	         nodes = nodo2.getChildNodes();
-	         
-	         for (int i = 0; i < nodes.getLength(); i++) {
-		            System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).getTextContent());
-		         }
-	         
-	         Node nodo3=nodes.item(6);
-	         System.out.println(nodo3.getNodeName());
-
-	         nodes = nodo3.getChildNodes();
-			      
-	         for (int i = 0; i < nodes.getLength(); i++) {
-		            System.out.println("Name: " + nodes.item(i).getNodeName()+" Value:"+nodes.item(i).getTextContent());
-		         }
-	         
-	         
-	         /**
-			StringBuffer result = new StringBuffer();
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-				System.out.println(line);
-			}
-			*/
 			} 
 			catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
@@ -98,11 +143,16 @@ public class DBPediaHTTPXML {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		 
+		
+		 return root;
 	}
 	
 	
-	public void testhttp2()
+	/**
+	 * Method to print the HTTP Request (Structure of the XML)
+	 * Testing purposes
+	 */
+	private void printHttpRequest()
 	{
 		try {
 			//QueryClass=person	
@@ -115,9 +165,7 @@ public class DBPediaHTTPXML {
 			//request.addHeader("User-Agent", USER_AGENT);
 			HttpResponse response;
 			
-	         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	
-			response = client.execute(request);
+	        response = client.execute(request);
 			
 			System.out.println("Response Code : " 
 		                + response.getStatusLine().getStatusCode());
@@ -138,16 +186,13 @@ public class DBPediaHTTPXML {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			} 
 	}
 	
 	public static void main(String[] args)
 	{
 		DBPediaHTTPXML db= new DBPediaHTTPXML();
-		db.testhttp();
+		db.printHttpRequest();
 		
 	}
 }
--- src/java/apolo/api/IDBpedia.java
+++ src/java/apolo/api/IDBpedia.java
@@ -1,5 +1,34 @@
 package apolo.api;
 
+import apolo.entity.IArtist;
+import apolo.entity.IRelease;
+import apolo.entity.ISong;
+
 public interface IDBpedia {
 
+	// --------------------------------------------------------
+	// Methods
+	// --------------------------------------------------------
+
+	/**
+	 * Get AdditionalInformation about a song
+	 * @param r, song to search in DBPedia
+	 * @return
+	 */
+	public ISong getAdditionalInformationSong(ISong s);
+	
+	/**
+	 * Get AdditionalInformation about a release
+	 * @param r, release to search in DBPedia
+	 * @return
+	 */
+	public IRelease getAdditionalInformationRelease(IRelease r);
+	
+	/**
+	 * Get AdditionalInformation about an artist
+	 * @param a, artist to search in DBPedia
+	 * @return
+	 */
+	public IArtist getAdditionalInformationArtist(IArtist a);
+	
 }
