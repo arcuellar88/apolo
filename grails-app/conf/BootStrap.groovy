3aff0bd9ac2b4d0b27f6cedbfcefcc6d766212bd
Truong Nguyen <truongnx15fl@gmail.com>
2015-05-20 07:02:59 +0200

    Update BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
+import java.io.File;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.FSDirectory;
+
+import apolo.querybuilder.Searcher
 import apolo.queryrefinement.Autocomplete
 import apolo.queryrefinement.NER
 import apolo.queryrefinement.SpellingCorrection
 import apolo.recommender.Recommender
+import apolo.msc.Global_Configuration
 
 class BootStrap {
 
@@ -10,6 +18,7 @@ class BootStrap {
 		servletContext.putAt("ner", new NER())
 		servletContext.putAt("spellchecker", new SpellingCorrection())
 		servletContext.putAt("recommender", new Recommender())
+		servletContext.putAt("indexSearcher", new IndexSearcher(DirectoryReader.open(FSDirectory.open(new File(Global_Configuration.INDEX_DIRECTORY)))))
     }
     def destroy = {
     }
