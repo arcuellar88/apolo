da716f3af15c7ca0223823d0ec3155cb41dbed7f
arcuellar88 <alejrodr@ulb.ac.be>
2015-06-05 00:57:59 +0200

    Recommender Pre compute

--- src/java/apolo/recommender/Recommender.java
+++ src/java/apolo/recommender/Recommender.java
@@ -1,6 +1,8 @@
 package apolo.recommender;
 
 import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
@@ -13,18 +15,26 @@ import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
-import org.apache.mahout.cf.taste.impl.model.jdbc.*;
-import org.apache.mahout.cf.taste.impl.recommender.*;
-import org.apache.mahout.cf.taste.impl.similarity.*;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.model.jdbc.SQL92JDBCDataModel;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.CachingItemSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
 import apolo.db.DWConnector;
-import apolo.entity.*;
-import apolo.msc.*;
+import apolo.entity.Artist;
+import apolo.entity.IArtist;
+import apolo.entity.IRankingItem;
+import apolo.entity.Ranking;
+import apolo.entity.RankingItem;
+import apolo.msc.Global_Configuration;
+import apolo.msc.Log;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class Recommender implements IRecommender {
 	// --------------------------------------------------------
@@ -36,6 +46,8 @@ public class Recommender implements IRecommender {
 
 	private ReloadFromJDBCDataModel dataModel;
 	private ItemSimilarity simCache;
+	private MysqlDataSource dataSource;
+	
 
 	// --------------------------------------------------------
 	// Constructor
@@ -62,12 +74,12 @@ public class Recommender implements IRecommender {
 			// MYSQL
 			if (database.equals(Global_Configuration.MYSQL)) {
 				
-				MysqlDataSource dataSource = new MysqlDataSource();
+				dataSource = new MysqlDataSource();
 				dataSource.setServerName(Global_Configuration.MYSQL_HOST);
 				dataSource.setUser(Global_Configuration.MYSQL_USER);
 				dataSource.setPassword(Global_Configuration.MYSQL_PWD);
 				dataSource.setDatabaseName(Global_Configuration.MYSQL_DB);
-
+				
 				// Performance settings:
 				dataSource.setCachePreparedStatements(true);
 				dataSource.setCacheCallableStmts(true);
@@ -76,9 +88,7 @@ public class Recommender implements IRecommender {
 				dataSource.setElideSetAutoCommits(true);
 
 				// Initialize data model
-				dataModel = new ReloadFromJDBCDataModel(new MySQLJDBCDataModel(
-						dataSource, "taste_preferences", "user_id", "item_id",
-						"preference", ""));
+				
 				
 			} else {
 				// ORACLE
@@ -107,10 +117,26 @@ public class Recommender implements IRecommender {
 
 	}
 
+	private void initializeDataModel() {
+
+		try
+		{
+		dataModel = new ReloadFromJDBCDataModel(new MySQLJDBCDataModel(
+				dataSource, "taste_preferences", "user_id", "item_id",
+				"preference", ""));
+		}catch (TasteException e) {
+			e.printStackTrace();
+		} 
+	}
+
 	@Override
 	public Ranking getRecommendation(IArtist a, int numRecommendations) {
 		//initialize(Global_Configuration.MYSQL);
-		Ranking r = getRecommendationNoPreCompute(a.getArtist_id(),
+		
+		//Ranking r = getRecommendationNoPreCompute(a.getArtist_id(),
+			//	numRecommendations);
+		//Use Precompute results
+		Ranking r = getRecommendationPreCompute(a.getArtist_id(),
 				numRecommendations);
 
 		return r;
@@ -128,6 +154,10 @@ public class Recommender implements IRecommender {
 		// Initialize empty recommendation Ranking
 		Ranking ranking = new Ranking();
 
+		if(dataModel==null)
+		{
+			initializeDataModel();
+		}
 		try {
 
 			// Initialize similarity matrix
@@ -180,6 +210,53 @@ public class Recommender implements IRecommender {
 	}
 
 	/**
+	 * Recommendation with pre-computed recommendation
+	 * @param artistID Id of the artist to search for recommendations for
+	 * @param numRecommendations Number of recommendations for the requested artist
+	 * @return
+	 */
+	public Ranking getRecommendationPreCompute(int artistID,
+			int numRecommendations) {
+		
+		// Initialize empty recommendation Ranking
+		Ranking ranking = new Ranking();
+		if(dataSource!=null)
+		{
+			try {
+			
+				PreparedStatement ps = dataSource.getConnection().prepareStatement("SELECT artists.*,similarity FROM apolo.artists join artist_recommender on FK_artist_rec=artist_id where fk_artist=? limit ?");
+				ps.setInt(1, artistID);
+				ps.setInt(2, numRecommendations);
+
+				ResultSet rs= ps.executeQuery();
+						
+				while(rs.next())
+				 {
+					Artist artist = new Artist();
+
+					artist.setArtist_id(rs.getInt("ARTIST_ID"));
+					artist.setGender(rs.getString("ARTIST_GENDER"));
+					artist.setName(rs.getString("ARTIST_NAME"));
+					artist.setType(rs.getString("ARTIST_TYPE"));
+					
+					IRankingItem ri=artist;
+					ri.setSimilarity(rs.getDouble("similarity"));
+					ranking.addRankingItem(ri);
+				 }
+			
+			}
+			
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	
+		return ranking;
+	}
+
+	/**
 	 * Pre computes the similarity for numArtist
 	 * @param numArtist
 	 * @param numSimilar
@@ -209,10 +286,8 @@ public class Recommender implements IRecommender {
 						.mostSimilarItems(itemId, numSimilar);
 				
 				for (RecommendedItem recommendedItem : recommendations) {
-					Log.println(itemId + "," + recommendedItem.getItemID()
-							+ "," + recommendedItem.getValue());
-					pw.println(itemId + "," + recommendedItem.getItemID() + ","
-							+ recommendedItem.getValue());
+					//Log.println(itemId + "," + recommendedItem.getItemID()	+ "," + recommendedItem.getValue());
+					pw.println(itemId + "," + recommendedItem.getItemID() + ","+ recommendedItem.getValue());
 				}
 				i++;
 			}
@@ -235,8 +310,8 @@ public class Recommender implements IRecommender {
 			public org.apache.mahout.cf.taste.recommender.Recommender buildRecommender(
 					DataModel datam) throws TasteException {
 				initializeItemSimilarity();
-				GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(
-						datam, simCache);
+				//GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(
+					//	datam, simCache);
 				return null;
 			}
 			};	
@@ -282,12 +357,14 @@ public class Recommender implements IRecommender {
 
 		a.setArtist_id(4425485);
 		Ranking r = rd.getRecommendation(a, 5);
-		System.out.println("\n" + r.getItems().size());
-		a.setArtist_id(4425485);
-		rd.getRecommendation(a, 5);
+			
+		Log.println("Items" + r.getItems().size());
+		for (int i = 0; i < r.getItems().size(); i++) {
+			Log.println(r.getItems().get(i).toString());
+		}
 		
 		
-		//rd.validator();
+				//rd.validator();
 	}
 
 	/*
